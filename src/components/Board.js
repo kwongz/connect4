dc406b33323bef13d01287ab40afeaf4377e0069
kwongz <kyle.wong917@gmail.com>
2022-01-19 16:18:03 -0500

    finshed game check logic

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,25 +1,23 @@
 import React, {useState} from 'react'
 import Column from './Column'
 
-function Board({boardMatrix}) {
+function Board({boardMatrix, setBoardMatrix}) {
 
     const [playerOneTurn, setPlayerOneTurn] = useState(true)
+    const [resetTracker, setResetTracker] = useState(false)
     const playerNumber = playerOneTurn ? 1 : 2
-    const [consecutiveDiagonalCoin, setConsecutiveDiagonalCoin] = useState(0)
 
     // toggle between player one and player 2
     const handleTurn = () => {
         if(playerOneTurn){
-            console.log('Player 1 went, now Player 2 turn')
             setPlayerOneTurn(false)
         } else {
-            console.log('Player 2 went, now Player 1 turn')
             setPlayerOneTurn(true)
         }
     }
     // receive the slot id and column id of where the coin was dropped, and update the board matrix 
     const getCoinPosition = (slotId, columnId) => {
-        boardMatrix[columnId][slotId] = playerNumber
+        setBoardMatrix(boardMatrix, boardMatrix[columnId][slotId] = playerNumber)
     }
 
     // check the if there are coin next to the dropped coin
@@ -30,7 +28,6 @@ function Board({boardMatrix}) {
     const checkWinner = (slotId, columnId) => {
         let count = 0
         let firstDirection = true
-        console.log(boardMatrix)
         const manuel = {
             leftUpDiag: [-1, 1],
             leftDownDiag: [1, -1],
@@ -44,58 +41,62 @@ function Board({boardMatrix}) {
         // Check Row winner
         checkDirectionWinner(slotId, columnId, manuel.right, manuel.left, count, firstDirection)
         // Check ColumnWinner
-        // checkDirectionWinner(slotId, columnId, manuel.up, manuel.down, count, firstDirection)
+        checkDirectionWinner(slotId, columnId, manuel.up, manuel.down, count, firstDirection)
         // check left to right diagonal \
-        // checkDirectionWinner(slotId, columnId, manuel.leftUpDiag, manuel.leftDownDiag, count, firstDirection)
+        checkDirectionWinner(slotId, columnId, manuel.leftUpDiag, manuel.leftDownDiag, count, firstDirection)
         // Check right to left diagonal /
-        // checkDirectionWinner(slotId, columnId, manuel.rightUpDiag, manuel.rightDownDiag, count, firstDirection)
-
+        checkDirectionWinner(slotId, columnId, manuel.rightUpDiag, manuel.rightDownDiag, count, firstDirection)
     }
 
     const checkDirectionWinner = (slotId, columnId, firstDirectionManuel, secondDirectionManuel, count, firstDirection) => {
-        console.log('New Function Execution')
-        console.log('checking current column',columnId)
-        // debugger;
 
         // Set the position value of the left and right coin of the current coin being checked
-        const firstDirectionColPos = columnId + firstDirectionManuel[1]
-        const firstDirectionSlotPos = slotId + firstDirectionManuel[0]
-        const secondDirectionColPos = columnId + secondDirectionManuel[1]
-        const secondDirectionSlotPos = slotId + secondDirectionManuel[0]
+        const firstDirectionColPos = columnId + firstDirectionManuel[0]
+        const firstDirectionSlotPos = slotId + firstDirectionManuel[1]
+        const secondDirectionColPos = columnId + secondDirectionManuel[0]
+        const secondDirectionSlotPos = slotId + secondDirectionManuel[1]
         // when more then 3 coins have been counted
         if(count === 3){
             return alert('winner')
         }
-
         // Check left side of coin if at the edge of the right side of board
-        if(firstDirectionColPos === boardMatrix.length ) {
+        if(firstDirectionColPos === boardMatrix.length || firstDirectionColPos < 0 ) {
             firstDirection = false;
+            count = 0
         }
-
         // Check Left side of coin
-        if(secondDirectionColPos > -1 && !firstDirection) {
-            console.log('checking left');
+        if(secondDirectionColPos < boardMatrix.length && secondDirectionColPos > -1 && !firstDirection) {
             if(boardMatrix[secondDirectionColPos][secondDirectionSlotPos] === playerNumber){
                 count++;
-                console.log('left',count);
                 checkDirectionWinner(secondDirectionSlotPos, secondDirectionColPos, firstDirectionManuel, secondDirectionManuel, count, firstDirection);
             }
         }
-        
         // check right side of coin
-        if(firstDirectionColPos < boardMatrix.length && firstDirection) {
+        if(firstDirectionColPos < boardMatrix.length && firstDirectionColPos > -1 && firstDirection) {
             if(boardMatrix[firstDirectionColPos][firstDirectionSlotPos] === playerNumber){
                 count++;
-                console.log('right',count);
                 checkDirectionWinner(firstDirectionSlotPos, firstDirectionColPos, firstDirectionManuel, secondDirectionManuel, count, firstDirection);
             } else {
-                console.log('check left');
                 firstDirection = false;
-                count = 0;
+                count = 0
                 checkDirectionWinner(slotId, columnId, firstDirectionManuel, secondDirectionManuel, count, firstDirection);
             }
         }
+    }
 
+    const buttonReset = () => {
+        setPlayerOneTurn(true)
+        setResetTracker(true)
+        setBoardMatrix([
+            [0,0,0,0,0,0],//0
+            [0,0,0,0,0,0],//1
+            [0,0,0,0,0,0],//2
+            [0,0,0,0,0,0],//3
+            [0,0,0,0,0,0],//4
+            [0,0,0,0,0,0],//5
+            [0,0,0,0,0,0] //6
+        ])
+        setResetTracker(false)
     }
 
 
@@ -108,6 +109,7 @@ function Board({boardMatrix}) {
                 getCoinPosition={getCoinPosition}
                 checkWinner={checkWinner}
                 columnId={0}
+                resetTracker={resetTracker}
             />
             <Column 
                 handleTurn={handleTurn} 
@@ -115,6 +117,7 @@ function Board({boardMatrix}) {
                 getCoinPosition={getCoinPosition}
                 checkWinner={checkWinner}
                 columnId={1}
+                resetTracker={resetTracker}
             />
             <Column 
                 handleTurn={handleTurn} 
@@ -122,6 +125,7 @@ function Board({boardMatrix}) {
                 getCoinPosition={getCoinPosition}
                 checkWinner={checkWinner}
                 columnId={2}
+                resetTracker={resetTracker}
             />
             <Column 
                 handleTurn={handleTurn} 
@@ -129,6 +133,7 @@ function Board({boardMatrix}) {
                 getCoinPosition={getCoinPosition}
                 checkWinner={checkWinner}
                 columnId={3}
+                resetTracker={resetTracker}
             />
             <Column 
                 handleTurn={handleTurn} 
@@ -136,9 +141,26 @@ function Board({boardMatrix}) {
                 getCoinPosition={getCoinPosition}
                 checkWinner={checkWinner}
                 columnId={4}
+                resetTracker={resetTracker}
+            />
+            <Column 
+                handleTurn={handleTurn} 
+                playerOneTurn={playerOneTurn}
+                getCoinPosition={getCoinPosition}
+                checkWinner={checkWinner}
+                columnId={5}
+                resetTracker={resetTracker}
+            />
+            <Column 
+                handleTurn={handleTurn} 
+                playerOneTurn={playerOneTurn}
+                getCoinPosition={getCoinPosition}
+                checkWinner={checkWinner}
+                columnId={6}
+                resetTracker={resetTracker}
             />
         </div>
-        <button>Reset</button>
+        <button onClick={buttonReset}>Reset</button>
     </>
     )
 }
